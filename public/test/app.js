87d84aeb8a98ef7a212a3c1283487e7edc269e71
Noah Appleby <appleby.n@husky.neu.edu>
2017-07-10 19:26:01 -0400

    remove unused code

--- public/test/app.js
+++ public/test/app.js
@@ -1,13 +1,4 @@
 (function() {
-
-    var connectionString = 'mongodb://127.0.0.1:27017/test'; // for local
-    if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
-        var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
-        var password = process.env.MLAB_PASSWORD_WEBDEV;
-        connectionString = 'mongodb://' + username + ':' + password;
-        connectionString += '@ds153412.mlab.com:53412/heroku_0wjf24b3'; // user yours
-    }
-
     angular
         .module("TestApp", [])
         .controller("TestController", TestController)
