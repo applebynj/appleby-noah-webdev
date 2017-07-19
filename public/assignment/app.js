9d84bf7fd18a6150d38e86542ef5ac1083ec471b
Noah Appleby <appleby.n@husky.neu.edu>
2017-07-19 17:30:12 -0400

    add login controller logic

--- public/assignment/app.js
+++ public/assignment/app.js
@@ -1,1 +1,23 @@
-angular.module("WamApp", []);
+var app = angular.module("WamApp", []);
+
+app.controller("loginController", loginController);
+
+function loginController($scope) {
+
+    var users = [
+        {_id: "123", username: "alice",    password: "alice",    firstName: "Alice",  lastName: "Wonder"  },
+        {_id: "234", username: "bob",      password: "bob",      firstName: "Bob",    lastName: "Marley"  },
+        {_id: "345", username: "charly",   password: "charly",   firstName: "Charly", lastName: "Garcia"  },
+        {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose",   lastName: "Annunzi" }
+    ];
+
+    $scope.login = function(user) {
+        for(var u in users) {
+            var _user = users[u];
+            if(user.username === _user.username &&
+            user.password === _user.password) {
+                $scope.welcomeUser = _user;
+            }
+        }
+    }
+}
