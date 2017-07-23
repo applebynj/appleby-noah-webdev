1e875885e90a3d72cdcd29e291005e886e1bc7df
Noah Appleby <appleby.n@husky.neu.edu>
2017-07-23 15:08:58 -0400

    move user controller logic to service

--- public/assignment/user/controllers/login.controller.client.js
+++ public/assignment/user/controllers/login.controller.client.js
@@ -3,31 +3,15 @@
         .module("WamApp")
         .controller("loginController", loginController)
 
-    var users = [
-        {_id: "123", username: "alice", password: "alice", firstName: "Alice", lastName: "Wonder"},
-        {_id: "234", username: "bob", password: "bob", firstName: "Bob", lastName: "Marley"},
-        {_id: "345", username: "charly", password: "charly", firstName: "Charly", lastName: "Garcia"},
-        {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose", lastName: "Annunzi"}
-    ];
-
-    function loginController($scope, $location) {
-
-        var users = [
-            {_id: "123", username: "alice", password: "alice", firstName: "Alice", lastName: "Wonder"},
-            {_id: "234", username: "bob", password: "bob", firstName: "Bob", lastName: "Marley"},
-            {_id: "345", username: "charly", password: "charly", firstName: "Charly", lastName: "Garcia"},
-            {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose", lastName: "Annunzi"}
-        ];
+    function loginController($scope, $location, userService) {
 
         $scope.login = function (user) {
-            for (var u in users) {
-                var _user = users[u];
-                if (user.username === _user.username &&
-                    user.password === _user.password) {
-                    $location.url("profile/" + _user._id);
-                }
+            user = userService.findUserByUsernameAndPassword(user.username, user.password);
+            if(user === null) {
+                $scope.errorMessage = "User not found";
+            } else {
+                $location.url("profile/" + user._id);
             }
-            $scope.errorMessage = "User not found";
         }
     }
 })();
