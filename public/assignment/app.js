b50d9e45626a28b059145e1c140476c0e5557a28
Noah Appleby <appleby.n@husky.neu.edu>
2017-07-23 12:46:46 -0400

    add user route param between login and profile

--- public/assignment/app.js
+++ public/assignment/app.js
@@ -1,6 +1,8 @@
 var app = angular.module("WamApp", ["ngRoute"]);
 
 app.controller("loginController", loginController);
+app.controller("profileController", profileController);
+
 
 app.config(configuration);
 
@@ -12,10 +14,17 @@ function configuration($routeProvider) {
         .when("/register", {
             templateUrl: "register.html"
         })
-        .when("/profile", {
+        .when("/profile/:userId", {
             templateUrl: "profile.html"
         })
 }
+
+var users = [
+    {_id: "123", username: "alice",    password: "alice",    firstName: "Alice",  lastName: "Wonder"  },
+    {_id: "234", username: "bob",      password: "bob",      firstName: "Bob",    lastName: "Marley"  },
+    {_id: "345", username: "charly",   password: "charly",   firstName: "Charly", lastName: "Garcia"  },
+    {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose",   lastName: "Annunzi" }
+];
 
 function loginController($scope, $location) {
 
@@ -31,9 +40,18 @@ function loginController($scope, $location) {
             var _user = users[u];
             if(user.username === _user.username &&
             user.password === _user.password) {
-                $location.url("profile");
+                $location.url("profile/" + _user._id);
             }
         }
         $scope.errorMessage = "User not found";
+    }
+}
+
+function profileController($scope, $routeParams) {
+    var userId = $routeParams["userId"];
+    for(var u in users) {
+        if(users[u]._id === userId) {
+            $scope.user = users[u];
+        }
     }
 }
