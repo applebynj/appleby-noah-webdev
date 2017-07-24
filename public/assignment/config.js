8b44de636edb1fc60679a782ea64cfe1ce011452
Noah Appleby <appleby.n@husky.neu.edu>
2017-07-24 12:41:17 -0300

    add page and widget routes to config

--- public/assignment/config.js
+++ public/assignment/config.js
@@ -5,6 +5,11 @@
 
     function configuration($routeProvider) {
         $routeProvider
+            .when("/", {
+                templateUrl: "user/templates/login.view.client.html",
+                controller: "loginController",
+                controllerAs: "model"
+            })
             .when("/login", {
                 templateUrl: "user/templates/login.view.client.html",
                 controller: "loginController",
@@ -15,20 +20,63 @@
                 controller: "registerController",
                 controllerAs: "model"
             })
-            .when("/profile/:userId", {
+            .when("/user/:uid", {
                 templateUrl: "user/templates/profile.view.client.html",
                 controller: "profileController",
                 controllerAs: "model"
             })
         //Website Routes
-            .when("/user/:userId/website", {
+            .when("/user/:uid/website", {
                 templateUrl: "website/templates/website-list.view.client.html",
                 controller: "websiteListController",
                 controllerAs: "model"
             })
-            .when("/user/:userId/website/new", {
+            .when("/user/:uid/website/new", {
                 templateUrl: "website/templates/website-new.view.client.html",
                 controller: "websiteNewController",
+                controllerAs: "model"
+            })
+            .when("/user/:uid/website/:wid", {
+                templateUrl: "website/templates/website-edit.view.client.html",
+                // controller: "",
+                controllerAs: "model"
+            })
+        //Page Routes
+            .when("/user/:uid/website/:wid/page", {
+                templateUrl: "website/templates/page-list.view.client.html",
+                // controller: "",
+                controllerAs: "model"
+            })
+            .when("/user/:uid/website/:wid/page/new", {
+                templateUrl: "website/templates/page-new.view.client.html",
+                // controller: "",
+                controllerAs: "model"
+            })
+            .when("/user/:uid/website/:wid/page/:pid", {
+                templateUrl: "website/templates/page-edit.view.client.html",
+                // controller: "",
+                controllerAs: "model"
+            })
+        //Widget Routes
+            .when("/user/:uid/website/:wid/page/:pid/widget", {
+                templateUrl: "website/templates/widget-list.view.client.html",
+                // controller: "",
+                controllerAs: "model"
+            })
+            .when("/user/:uid/website/:wid/page/:pid/widget/new", {
+                templateUrl: "website/templates/widget-new.view.client.html",
+                // controller: "",
+                controllerAs: "model"
+            })
+            .when("/user/:uid/website/:wid/page/:pid/widget/:wgid", {
+                templateUrl: "website/templates/widget-edit.view.client.html",
+                // controller: "",
+                controllerAs: "model"
+            })
+        //Default Route
+            .otherwise({
+                templateUrl: "user/templates/login.view.client.html",
+                controller: "loginController",
                 controllerAs: "model"
             })
     }
