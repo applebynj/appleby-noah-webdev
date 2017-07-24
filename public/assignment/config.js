f4b1da0d0ca4f2471e080350565680eecd891db3
Noah Appleby <appleby.n@husky.neu.edu>
2017-07-24 13:51:54 -0300

    update routing to new controllers

--- public/assignment/config.js
+++ public/assignment/config.js
@@ -7,70 +7,70 @@
         $routeProvider
             .when("/", {
                 templateUrl: "views/user/templates/login.view.client.html",
-                controller: "loginController",
+                controller: "LoginController",
                 controllerAs: "model"
             })
             .when("/login", {
                 templateUrl: "views/user/templates/login.view.client.html",
-                controller: "loginController",
+                controller: "LoginController",
                 controllerAs: "model"
             })
             .when("/register", {
                 templateUrl: "views/user/templates/register.view.client.html",
-                controller: "registerController",
+                controller: "RegisterController",
                 controllerAs: "model"
             })
             .when("/user/:uid", {
                 templateUrl: "views/user/templates/profile.view.client.html",
-                controller: "profileController",
+                controller: "ProfileController",
                 controllerAs: "model"
             })
         //Website Routes
             .when("/user/:uid/website", {
                 templateUrl: "views/website/templates/website-list.view.client.html",
-                controller: "websiteListController",
+                controller: "WebsiteListController",
                 controllerAs: "model"
             })
             .when("/user/:uid/website/new", {
                 templateUrl: "views/website/templates/website-new.view.client.html",
-                controller: "websiteNewController",
+                controller: "NewWebsiteController",
                 controllerAs: "model"
             })
             .when("/user/:uid/website/:wid", {
                 templateUrl: "views/website/templates/website-edit.view.client.html",
-                // controller: "",
+                controller: "EditWebsiteController",
                 controllerAs: "model"
             })
         //Page Routes
             .when("/user/:uid/website/:wid/page", {
                 templateUrl: "views/website/templates/page-list.view.client.html",
-                // controller: "",
+                controller: "PageListController",
                 controllerAs: "model"
             })
             .when("/user/:uid/website/:wid/page/new", {
                 templateUrl: "views/website/templates/page-new.view.client.html",
-                // controller: "",
+                controller: "NewPageController",
                 controllerAs: "model"
             })
             .when("/user/:uid/website/:wid/page/:pid", {
                 templateUrl: "views/website/templates/page-edit.view.client.html",
-                // controller: "",
+                controller: "EditPageController",
                 controllerAs: "model"
             })
         //Widget Routes
             .when("/user/:uid/website/:wid/page/:pid/widget", {
                 templateUrl: "views/website/templates/widget-list.view.client.html",
-                // controller: "",
+                controller: "WidgetListController",
                 controllerAs: "model"
             })
             .when("/user/:uid/website/:wid/page/:pid/widget/new", {
                 templateUrl: "views/website/templates/widget-new.view.client.html",
-                // controller: "",
+                controller: "NewWidgetController",
                 controllerAs: "model"
             })
             .when("/user/:uid/website/:wid/page/:pid/widget/:wgid", {
                 templateUrl: "views/website/templates/widget-edit.view.client.html",
-                // controller: "",
+                controller: "EditWidgetController",
                 controllerAs: "model"
             })
         //Default Route
