05a99d954727eb18480ebfdf39fa0a776cb54197
Noah Appleby <appleby.n@husky.neu.edu>
2017-07-31 07:20:23 -0400

    add beginning server logic

---
+++ assignment/app.js
@@ -0,0 +1,20 @@
+var app = require("../express");
+
+var users = [
+    {_id: "123", username: "alice", password: "alice", firstName: "Alice", lastName: "Wonder", email:"alice@wonder.com"},
+    {_id: "234", username: "bob", password: "bob", firstName: "Bob", lastName: "Marley", email:"bob@marley.com"},
+    {_id: "345", username: "charly", password: "charly", firstName: "Charly", lastName: "Garcia", email:"charly@garcia.com"},
+    {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose", lastName: "Annunzi", email: "jose@annunzi.com"}
+];
+
+app.get("/users", function (req, res) {
+    res.send(users);
+});
+
+app.get("/users/:userId", function (req, res) {
+    for(var u in users) {
+        if(users[u]._id === req.params.userId) {
+            res.send(users[u]);
+        }
+    }
+});
