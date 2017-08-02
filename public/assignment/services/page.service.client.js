aef81eef2c341642c05f41d267fa4891f3c03ede
Noah Appleby <appleby.n@husky.neu.edu>
2017-08-02 00:30:49 -0400

    remove data arrays from client services

--- public/assignment/services/page.service.client.js
+++ public/assignment/services/page.service.client.js
@@ -5,12 +5,6 @@
 
     function PageService($http) {
 
-        var pages = [
-            { "_id": "321", "name": "Post 1", "websiteId": "456", "title": "Lorem" },
-            { "_id": "432", "name": "Post 2", "websiteId": "456", "title": "Lorem" },
-            { "_id": "543", "name": "Post 3", "websiteId": "456", "title": "Lorem" }
-        ];
-
         var api = {
             'createPage' : createPage,
             'findPagesByWebsiteId' : findPagesByWebsiteId,
